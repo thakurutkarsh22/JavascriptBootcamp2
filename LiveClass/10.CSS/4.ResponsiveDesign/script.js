670ae798a5eb7eda0ceedd1c83487f4715a86c9a
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-11-16 12:43:38 +0530

    responsive design

---
+++ LiveClass/10.CSS/4.ResponsiveDesign/script.js
@@ -0,0 +1,43 @@
+// Responsive design
+
+// 1. Using some library -> Tailwind, bootstrap.
+// 2. using media queries 
+
+// Ground rules of responsive designs
+/*
+    1. Use of Absolute units should be less (px, in, cm .... ).
+    2. use Relative units (rem, em, %, vw, vh).
+    3. container should be fluid (flex );
+    4. Most of the times do not fix WIdth and height.
+    5. Design mobile first 
+        -> add breakpoints 
+
+    Breakpoints -> a point where the layout changes start 
+    
+    For Mobile Devices – 320px — 480px.
+    iPads and Tablets – 481px — 768px.
+    Laptops and small screen – 769px — 1024px.
+    Large screens and Desktops – 1025px — 1200px.
+    TV and Extra Large Screens – 1201px and more.
+
+    
+    RULES  to define Media Queries 
+
+    @media media-type media-feature {
+    
+        // normal css
+
+        h1 {
+            background-color: red
+        }
+    }
+
+    media-type -> optinal 
+    media-feature -> (min-width: 400px) 
+     media-feature -> (min-width: 400px) and (max-width: 800px) 
+
+
+
+
+
+*/
