97cf8f28a224852561825850e83439a791c46f52
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-11-17 12:54:10 +0530

    important information

---
+++ ImportantLinks/index.js
@@ -0,0 +1,6 @@
+// html css - w3school
+// JS  -> jsinfo 
+// Critical rendering path: https://www.udacity.com/course/browser-rendering-optimization--ud860
+// REFLOW AND REPAINT 
+// performnce - https://web.dev/learn/performance
+// web - https://web.dev/
