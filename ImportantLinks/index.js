160c9f39da8537e493414bceaf3326af83a0477b
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-12-08 22:08:32 +0530

    imp info

--- ImportantLinks/index.js
+++ ImportantLinks/index.js
@@ -1,6 +1,15 @@
 // html css - w3school
 // JS  -> jsinfo 
+// GARBAGE COLLECTOIN: https://v8.dev/blog/trash-talk
 // Critical rendering path: https://www.udacity.com/course/browser-rendering-optimization--ud860
 // REFLOW AND REPAINT 
 // performnce - https://web.dev/learn/performance
 // web - https://web.dev/
+
+
+
+
+
+
+// V8 engine (web compliler ) - https://v8.dev/
+// 
